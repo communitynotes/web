75cc8376d936cee2bf003dc29e83e24b35cc9a90
Michal Jach <michaljach@gmail.com>
2025-02-01 23:03:46 +0100

    pipeline fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const signIn = async () => {
     const deviceCode = await fetch("https://github.com/login/device/code", {
       method: "POST",
